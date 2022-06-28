364321ac471e68fef30170bee2d354642ad3de17
Juliane Ferreira <71344283+F-Juliane@users.noreply.github.com>
2022-06-27 23:43:15 -0300

    Add files via upload

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,12 +2,6 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "123456",
-    DB: "db_planets",
+    DB: "db.planets",
     dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
   };
